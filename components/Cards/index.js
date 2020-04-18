c3f7364a5855d14a2b06c7a526f8bd58b6ea54fc
rauleflores <58542850+rauleflores@users.noreply.github.com>
2020-04-17 19:09:53 -0700

    completed MVP

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,3 +18,60 @@
 // </div>
 //
 // Use your function to create a card for each of the articles and add the card to the DOM.
+
+const cardsCont = document.querySelector('.cards-container')
+console.log(cardsCont)
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles').then( response => {
+    cardInfo(response)
+}).catch( err => { console.log(err) })
+
+function cardInfo (response) {
+        const articles = [response.data.articles];
+        console.log(articles)
+        articles.forEach( topics => {
+            console.log(topics)
+            topicsArr = Object.entries(topics)
+            topicsArr.forEach( items => {
+                const arrays = items[1]
+                arrays.forEach(item => {
+                //    console.log(Cards(item))
+                   const cardI = Cards(item)
+                   console.log(cardI)
+                   cardsCont.appendChild(cardI)
+                })
+            })
+        })
+}
+
+// cardInfo();
+
+function Cards( obj ) {
+    const cardDiv = document.createElement('div')
+    cardDiv.classList.add('card')
+
+    const headline = document.createElement('div')
+    headline.classList.add('headline')
+    headline.textContent = obj.headline
+
+    const author = document.createElement('div')
+    author.classList.add('author')
+
+    const imgCont = document.createElement('div')
+    imgCont.classList.add('img-container')
+
+    const image = document.createElement('img')
+    image.src = obj.authorPhoto
+    
+    const by = document.createElement('span')
+    by.textContent = obj.authorName
+
+    imgCont.appendChild(image)
+    author.appendChild(imgCont)
+    author.appendChild(by)
+    cardDiv.appendChild(headline)
+    cardDiv.appendChild(author)
+
+    return cardDiv
+
+}
